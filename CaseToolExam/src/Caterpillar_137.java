bc34d145af7835a3086882452c006a71f3e9c7c6
muska <muska@LAPTOP-DB02CP52>
2022-06-13 13:03:49 +1400

    Commit My First Program

---
+++ CaseToolExam/src/Caterpillar_137.java
@@ -0,0 +1,15 @@
+
+public class Caterpillar_137 {
+	int moves,cabbages;
+	Caterpillar_137(int moves,int cabbage)
+	{
+		this.moves=moves;
+		this.cabbages=cabbage;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
